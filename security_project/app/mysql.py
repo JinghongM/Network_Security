5da675f7718a6622f6ac0fd3ece9827805ab2b4e
JinghongM <jm7012@nyu.edu>
2018-05-10 21:25:15 -0400

    Add files via upload

--- security_project/app/mysql.py
+++ security_project/app/mysql.py
@@ -1,5 +1,5 @@
 from flask_mysqldb import MySQLdb
-
+import json
 def connect():
 	conn = MySQLdb.connect(host="localhost",user="root",passwd="Jason19940705",db="security_test")
 	cur = conn.cursor()
@@ -7,10 +7,12 @@ def connect():
 def query(cur,sql_query):
 	print(sql_query)
 	num = cur.execute(sql_query)
-	print(num)
-	print(type(num))
-	if num == 0:
+	row_headers=[x[0] for x in cur.description]
+	results=cur.fetchall()
+	json_data=[]
+	for result in results:
+		json_data.append(dict(zip(row_headers,result)))
+	if len(json_data) == 0:
 		raise Exception('No value selected')
 	else:
-		data=cur.fetchall()
-		return data
+		return json.dumps(json_data)
--- security_project/app/routes.py
+++ security_project/app/routes.py
@@ -2,7 +2,7 @@ from app import app
 from flask import render_template,flash,redirect
 from app.forms import LoginForm
 from app.mysql import connect,query
-
+import json
 @app.route('/',methods=['POST','GET'])
 @app.route('/login',methods=['POST','GET'])
 def login():
@@ -10,10 +10,13 @@ def login():
 	if form.validate_on_submit():
 		sql_query = "select user_information from user_profile where user_name='"+form.username.data+"'" + "and password='" + form.password.data+"'";
 		try:
-			result = query(connect(),sql_query)
-			return render_template('result.html',user_name = form.username.data,information=result[0][0])
+			result = json.loads(query(connect(),sql_query))
+			print(result)
+			print(type(result))
+			print(len(result))
+			return render_template('result.html',user_name = form.username.data,information=result)
 		except:
 			flash("Invalid username/password")
 			return redirect('/login')
 	return render_template('login.html',form=form)
-username=' ' or '1' = '1' and password=' ' or '1' = '1'
+
