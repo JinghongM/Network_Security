fee233c61b4fb4b579b2dbb04d6cb3b8a95e0acf
Wei Lin <123333ster@gmail.com>
2018-05-11 17:06:42 -0400

    Changes

--- commands-line.sh
+++ commands-line.sh
@@ -1,15 +1,7 @@
 sudo apt-get update
+# Install mysql:
 sudo apt-get install mysql-server
-cd Network_Security
-mysql -u root -p < ./database.sql
 
-Check table:
-use security_test;
-select * from user_profile;
-
-Install pip3:
-sudo apt-get install python3-pip
-sudo apt-get install libmysqlclient-dev
-sudo pip3 install flask
-sudo pip3 install flask_mysqldb
-sudo pip3 install flask_wtf
+# Install pip3:
+sudo apt-get install python3-pip libmysqlclient-dev
+sudo pip3 install flask flask_mysqldb flask_wtf
